1932c9ff555d6c99acbaeea752c70a545a84bcf5
Sam <98867424+Shyt562@users.noreply.github.com>
2023-01-25 04:04:27 +0800

    Update calc.js

--- commands/calc.js
+++ commands/calc.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js'
 const { evaluate } = require('mathjs')
 
 module.exports = {
