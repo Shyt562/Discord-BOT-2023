6dafe11a63ae769ad371d304ab2868e3239ee943
默軒 <98867424+Shyt562@users.noreply.github.com>
2023-01-25 03:37:14 +0800

    Update calc.js

--- commands/calc.js
+++ commands/calc.js
@@ -4,10 +4,10 @@ const { evaluate } = require('mathjs')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('calc')
-        .setDescription('Calculate a math problem')
+        .setDescription('計算數學題')
         .addStringOption(option =>
             option.setName('problem')
-                .setDescription('The math problem to solve')
+                .setDescription('要解決的數學問題')
                 .setRequired(true)),
     
     async execute(client, interaction) {
@@ -17,13 +17,13 @@ module.exports = {
         try {
             answer = evaluate(problem)
         } catch (error) {
-            interaction.reply({ content: 'Cannot resolve the math problem', ephemeral: true })
+            interaction.reply({ content: '無法解決此問題', ephemeral: true })
             return
         }
 
         const embed = new EmbedBuilder()
             .setTitle('Calculator')
-            .setDescription(`**Problem:** \`${problem}\` = ${answer}`)
+            .setDescription(`**說明:** \`${problem}\` = ${answer}`)
             .setColor(0x4B0082)
         
         await interaction.reply({ embeds: [embed] })
