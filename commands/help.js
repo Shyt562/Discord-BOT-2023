43f7e95845715b1a520735c962f9bc9cb6141224
默軒 <98867424+Shyt562@users.noreply.github.com>
2023-01-25 03:39:56 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,18 +3,18 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Get help with the bot'),
+        .setDescription('機器人的指令清單'),
 
     async execute(client, interaction) {
         const embed = new EmbedBuilder()
             .setTitle('幫助頁面')
             .setDescription('這是幫助頁面，你可以在這裡找到所有指令')
             .addFields({
-                name: '資訊', value: '`/help`, `/user-info`, `/role-info`'
+                name: '資訊',`/help`, `/user-info`, `/role-info`'
             }, {
-                name: '實用/娛樂', value: '`/calc`, `/say`, `/choose`, `/uuid`, `/translate`'
+                name: '實用/娛樂',`/calc`, `/say`, `/choose`, `/uuid`, `/translate`'
             }, {
-                name: '管理', value: '`/ban`'
+                name: '管理',`/ban`'
             })
             .setColor(0x5865F2)
 
