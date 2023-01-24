0f2dfb282b16120c28ec9ae9e207fe7ec4108075
默軒 <98867424+Shyt562@users.noreply.github.com>
2023-01-25 03:35:05 +0800

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,26 +3,26 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
-        .setDescription('Ban someone from server')
-        .addUserOption(option => option.setName('user').setDescription('The user to ban').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('The reason to ban')),
+        .setDescription('從服務器停權用戶')
+        .addUserOption(option => option.setName('user').setDescription('用戶').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('原因')),
 
     async execute(client, interaction) {
-        if (!interaction.member.permissions.has('BAN_MEMBERS')) return interaction.reply({ content: 'You don\'t have permission to ban members', ephemeral: true })
+        if (!interaction.member.permissions.has('BAN_MEMBERS')) return interaction.reply({ content: '您沒有權限可以停權用戶', ephemeral: true })
 
         const user = interaction.options.getUser('user')
 
-        if (!user) return interaction.reply({ content: 'Please mention a user to ban', ephemeral: true })
+        if (!user) return interaction.reply({ content: '請選擇你要停權的用戶', ephemeral: true })
 
         const member = interaction.guild.members.cache.get(user.id)
 
-        if (member.id === interaction.user.id) return interaction.reply({ content: 'You can\'t ban yourself', ephemeral: true })
+        if (member.id === interaction.user.id) return interaction.reply({ content: '你無法停權你自己', ephemeral: true })
 
-        if (member.id === client.user.id) return interaction.reply({ content: 'You can\'t ban me', ephemeral: true })
+        if (member.id === client.user.id) return interaction.reply({ content: '你無法將我給停權', ephemeral: true })
 
-        if (member.roles.highest.position >= interaction.member.roles.highest.position) return interaction.reply({ content: 'You can\'t ban this user', ephemeral: true })
+        if (member.roles.highest.position >= interaction.member.roles.highest.position) return interaction.reply({ content: '您無法停權此用戶', ephemeral: true })
 
-        const reason = interaction.options.getString('reason') || 'No reason provided'
+        const reason = interaction.options.getString('reason') || '沒有理由'
 
         const embed = new EmbedBuilder()
             .setTitle('已封禁使用者')
@@ -35,7 +35,7 @@ module.exports = {
             await interaction.reply({ embeds: [embed] })
         } catch (err) {
             console.log(err)
-            return interaction.reply({ content: 'I was unable to ban the member', ephemeral: true })
+            return interaction.reply({ content: '我無法停權該用戶', ephemeral: true })
         }
     }
 }
